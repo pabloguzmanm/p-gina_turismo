99032fdbd52b12a3fdcb5c6add2e7dfd086a886e
Mauricio Fuentes <mauriciofb@gmail.com>
2024-08-23 19:58:32 -0400

    Comentarios en configuraciones

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  "tabWidth": 2,
-  "useTabs": false,
-  "semi": true,
-  "singleQuote": true,
-  "trailingComma": "none",
-  "printWidth": 80,
-  "bracketSpacing": true
-}
+  tabWidth: 2, // cantidad de espacio de tabulación
+  useTabs: false, // no usa caracter de tabulación
+  semi: true, // añade ; al final de cada declaración
+  singleQuote: true, // comillas simples
+  trailingComma: 'none', // coma al final de objetos y listas
+  printWidth: 80, // ancho de cada línea
+  bracketSpacing: true // espaciado entre corchetes de objetos
+};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,23 +7,23 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   {
-    ...html.configs['flat/recommended'],
-    files: ['**/*.html'],
+    ...html.configs['flat/recommended'], // configuraciones recomendadas por librería
+    files: ['**/*.html'], // analizamos archivos html
     plugins: {
-      '@html-eslint': html
+      '@html-eslint': html // plugin para analizar HTML
     },
     languageOptions: {
-      parser: htmlParser
+      parser: htmlParser // analizar de código HTML
     },
     rules: {
-      '@html-eslint/indent': ['error', 2],
-      '@html-eslint/no-duplicate-id': 'error',
-      '@html-eslint/no-duplicate-attrs': 'error',
-      '@html-eslint/require-doctype': 'error',
-      '@html-eslint/require-li-container': 'error',
-      '@html-eslint/require-meta-charset': 'error',
-      '@html-eslint/no-script-style-type': 'error',
-      '@html-eslint/require-closing-tags': ['error', { selfClosing: 'always' }]
+      '@html-eslint/indent': ['error', 2], // indentación con 2 espacios
+      '@html-eslint/no-duplicate-id': 'error', // no duplicación de id
+      '@html-eslint/no-duplicate-attrs': 'error', // no duplicación de atributos
+      '@html-eslint/require-doctype': 'error', // se requiere etiqueta doctype
+      '@html-eslint/require-li-container': 'error', // li deben estar dentro de menu, ul, ol
+      '@html-eslint/require-meta-charset': 'error', // charset (conjunto de caracteres)
+      '@html-eslint/no-script-style-type': 'error', // no agregar estilos en línea
+      '@html-eslint/require-closing-tags': ['error', { selfClosing: 'always' }] // requiere cierre de etiquetas, o barra diagonal en autocierre
     }
   }
 ];
